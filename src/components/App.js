a1eb280dc922f66344da0b8d4f52b7d63c6617b2
Ahmed Nabil <bplahmed28d@gmail.com>
2024-09-01 14:40:13 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import About from "../components/About";
+import { city, image, username } from "../data/user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image} />
     </div>
   );
 }
